bf8940fff012a3022f943ef0710e7b4bac327810
Esen Meredow <esca656585@gmail.com>
2023-12-15 16:05:08 +0500

    updated

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/state/standards', [App\Http\Controllers\ApiControllers\Standart\ApiStandartController::class, 'index'])->middleware('auth.basic')->name('api.state.standards');
+// Route::post('/state/standards/error', [App\Http\Controllers\ApiControllers\Standart\ApiStandartController::class, 'index'])->middleware('auth.basic')->name('api.state.standards');
 
     

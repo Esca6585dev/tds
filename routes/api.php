e469261b554df82deb21ec0a9837d94e1d4f6868
Esen Meredow <esca656585@gmail.com>
2023-12-18 15:58:04 +0500

    updated

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,4 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// Route::post('/state/standards/error', [App\Http\Controllers\ApiControllers\Standart\ApiStandartController::class, 'index'])->middleware('auth.basic')->name('api.state.standards');
-
-    
+*/ 

8e992678f54c54db6c8d0185a6c145a24efb3e33
Esen Meredow <esca656585@gmail.com>
2024-01-02 11:24:12 +0500

    updated fixed style bugs

--- resources/views/user-panel/works-section.blade.php
+++ resources/views/user-panel/works-section.blade.php
@@ -28,7 +28,7 @@
   
           <div class="section__container__tab__bottom">
             <div class="section__container__tab__title">
-              {{ $row->{ 'name_' . app()->getlocale() } }}
+              {{ Str::limit($row->{ 'name_' . app()->getlocale() }, 100) }}
             </div>
             <div class="section__container__tab__link">
               <a href="{{ route('works.id', [app()->getlocale(), $row->id, Str::slug($row->{ 'name_' . app()->getlocale() }) ]) }}">

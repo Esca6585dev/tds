ab62365eb6fe716b7a41564420e3a22cf205dd0f
Esen Meredow <esca656585@gmail.com>
2024-01-05 16:54:21 +0500

    updated

--- resources/views/components/user/head.blade.php
+++ resources/views/components/user/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8" />
 <meta name="mailru-domain" content="OdyTaiNToK7KPdIn" />
 <meta name="google-site-verification" content="qI5UoqtYMl52aj8EkscnMcAbyBbADQcUuvCA-HU3lkk" />
+<meta name="google-adsense-account" content="ca-pub-4012886709218127">
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />

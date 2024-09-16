c21990ea685f772c0b85bf80bfabc6622f68bf42
Esen Meredow <esca656585@gmail.com>
2024-09-16 19:10:23 +0500

    updated after hack

--- resources/views/components/user/script.blade.php
+++ resources/views/components/user/script.blade.php
@@ -2,14 +2,3 @@
 <script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
 <script src="{{ asset('js/script.js') }}"></script>
 <script async defer data-website-id="8db27285-cc4f-45f6-96f1-46ab17fc8c01" src="https://metrics.com.tm/ynamly.js"></script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4012886709218127"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-format="fluid"
-     data-ad-layout-key="+c+qf-i-d+cr"
-     data-ad-client="ca-pub-4012886709218127"
-     data-ad-slot="9006980066"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
